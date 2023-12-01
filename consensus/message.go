f55e159d839e2aa922de757d9c6ac0580b387c61
nenadmilosevic95 <nenad@informal.systems>
2023-12-01 10:38:51 +0100

    marshalling changed

--- consensus/message.go
+++ consensus/message.go
@@ -122,7 +122,7 @@ func MessageFromBytes(buffer []byte) *Message {
 	mType := int16(buffer[1])
 	var epoch int64
 	var height int64
-	if mType != QUIT_EPOCH && mType != CERTIFICATE && mType != DELTA_REQUEST && mType != DELTA_RESPONSE {
+	if mType != QUIT_EPOCH && mType != DELTA_REQUEST && mType != DELTA_RESPONSE {
 		epoch = int64(encoding.Uint64(buffer[2:]))
 	}
 	index := 10
@@ -169,8 +169,8 @@ func MessageFromBytes(buffer []byte) *Message {
 		certificate = CertificateFromBytes(buffer[2:])
 		epoch = certificate.Epoch
 	case CERTIFICATE:
-		certificate = CertificateFromBytes(buffer[2:])
-		epoch = certificate.Epoch
+		certificate = CertificateFromBytes(buffer[index:])
+		index += certificate.ByteSize()
 	case DELTA_REQUEST, DELTA_RESPONSE:
 		payload = buffer[2 : len(buffer)-2]
 		index = len(buffer) - 2
@@ -231,7 +231,7 @@ func (m *Message) ByteSize() int {
 	case QUIT_EPOCH:
 		return 2 + m.Certificate.ByteSize()
 	case CERTIFICATE:
-		return 2 + m.Certificate.ByteSize()
+		return 2 + m.Certificate.ByteSize() + 8
 	case DELTA_REQUEST, DELTA_RESPONSE:
 		return 2 + len(m.payload) + 2
 	default:
@@ -268,7 +268,7 @@ func (m *Message) MarshallTo(buffer []byte) {
 	var n int
 	buffer[0] = MessageCode
 	buffer[1] = byte(m.Type) // 2 bytes
-	if m.Type != QUIT_EPOCH && m.Type != CERTIFICATE && m.Type != DELTA_REQUEST && m.Type != DELTA_RESPONSE {
+	if m.Type != QUIT_EPOCH && m.Type != DELTA_REQUEST && m.Type != DELTA_RESPONSE {
 		encoding.PutUint64(buffer[2:], uint64(m.Epoch))
 	}
 	index := 10
@@ -316,7 +316,8 @@ func (m *Message) MarshallTo(buffer []byte) {
 	case QUIT_EPOCH:
 		n = m.Certificate.MarshallTo(buffer[2:])
 	case CERTIFICATE:
-		n = m.Certificate.MarshallTo(buffer[2:])
+		n = m.Certificate.MarshallTo(buffer[index:])
+		index += m.Certificate.ByteSize()
 	case DELTA_REQUEST, DELTA_RESPONSE:
 		copy(buffer[2:], m.payload)
 		index = 2 + len(m.payload)
